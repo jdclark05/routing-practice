ba16a48a0af05e86f2facb1df8816e23231ec966
James Clark <jdclark05@gmail.com>
2021-05-10 20:14:13 -0400

    intial commit

---
+++ src/components/Hello.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+
+const Hello = (props) => {
+    return(
+        <div className="pageContainer">
+            <div>
+                <h1>Hello!</h1>
+            </div>
+        </div>
+    )
+}
+
+
+export default Hello;
---
+++ src/components/Page4.js
@@ -0,0 +1,16 @@
+import '../App.css';
+
+const Page4 = (props) => {
+    let int = props[""*""];
+
+    return(
+        <div className="pageContainer">
+            <div>
+                <h1>The number is: {int}!</h1>
+            </div>
+        </div>
+    )
+}
+
+
+export default Page4;
